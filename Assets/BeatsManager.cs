8859ab0366bed66a9cccf134ebe213f91fd63916
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2023-02-03 10:15:19 -0500

    have flashy light every beat

---
+++ Assets/BeatsManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatsManager : MonoBehaviour
+{
+    public float BeatsTimer;
+    public TextMeshProUGUI randomText;
+    public float beatTempo;
+    public AkAmbient BGMusic;
+    public Image image;
+    public bool doAltColor;
+
+
+    private void Start()
+    {
+        beatTempo = 60f / beatTempo;
+    }
+
+    void Update()
+    {
+        if (BGMusic.enabled)
+        {
+            BeatsTimer = BeatsTimer + Time.deltaTime;
+            if (BeatsTimer >= (beatTempo * 2))
+            {
+                BeatsTimer = 0f;
+                doAltColor = doAltColor ? false : true;
+                image.color = doAltColor ? new Color32(100, 0, 0, 255) : new Color32(0, 100, 0, 255);
+            }
+            randomText.text = BeatsTimer.ToString();
+        }
+        
+    }
+
+    public void StartMusic()
+    {
+        BGMusic.enabled = true;
+    }
+}
