16a50f8720b988724583b1f1b6c98befc7ae625e
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2023-02-04 00:12:33 -0500

    good enough for the night

--- Assets/LavaFloorManager.cs
+++ Assets/LavaFloorManager.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class LavaFloorManager : MonoBehaviour
 {
     public Transform lavaFloorTransform;
+    public GameObject lavaParticle;
+    public Transform particleGenerationArea;
 
+    public void Start()
+    {
+        for(var i = 0; i <= 50; i++ )
+        {
+            GameObject gameObj = Instantiate(lavaParticle, particleGenerationArea);
+            gameObj.transform.position = new Vector3(gameObj.transform.position.x + Random.Range(-50.5f, 15.4f), gameObj.transform.position.y, gameObj.transform.position.z);
+        }
+    }
     public void ScaleUpLavaFloor()
     {
         lavaFloorTransform.transform.localScale = new Vector3(lavaFloorTransform.transform.localScale.x, lavaFloorTransform.transform.localScale.y + .07f, lavaFloorTransform.transform.localScale.z);
